020ee41bd9c7460db8c08319fb04112606a225c0
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-10 17:02:38 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReverseAuction::Application.config.secret_key_base = 'bbdfba8fe8fe0eaa73952fc9283ba4fcd0bee5b00b149c0d7fec80465f8c31878570cfe27d8f28b7764eb857854e7bc8eaf2e3c79acf3e596c93b571d6b86cbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReverseAuction::Application.config.session_store :cookie_store, key: '_ReverseAuction_session'
