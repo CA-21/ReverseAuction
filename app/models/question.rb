a3a933f43185e4adbad22447a37d07d9cf6e9981
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-13 14:53:19 +0530

    Adds title, content validations to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 20, maximum: 150 }
+  validates :content, presence: true, length: { minimum: 200, maximum: 5000}
+
 end
