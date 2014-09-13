1ecd552153ac0e31b5e55a3f6a1d8bce40032c60
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-13 14:09:39 +0530

    Adds Questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ReverseAuction::Application.routes.draw do
 
   root 'static_pages#index'
 
+  resources :questions
+
   match '/help',      to: 'static_pages#help',      via: 'get'
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
