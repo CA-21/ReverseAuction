ec525772e351a390cba209850122a87a4a105b55
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-13 20:03:32 +0530

    Polishes the interface

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,13 @@ module ApplicationHelper
     end
   end
 
+  def gravatar_for(user, options = { size: 100 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'img-responsive')
+  end
+
 
   # Defines application-wide setting for application name. Change the app-name once
   # to get it reflected all over the application.
