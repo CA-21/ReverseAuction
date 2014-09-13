6537c1108ca01e2f5f5e5a3ddb3265d6353273c6
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-13 14:45:02 +0530

    Adds CRUD actions to Questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,26 @@
 class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    @question.save
+    redirect_to @question
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :content)
+    end
+
 end
