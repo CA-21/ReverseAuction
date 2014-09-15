98a597f83342ac31027f45f52929edbe32da8a80
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-15 19:10:53 +0530

    Adds admin privacy on listing pages.

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -6,10 +6,16 @@ class LawyersController < ApplicationController
 
   def show
     @lawyer = Lawyer.find(params[:id])
+
+    if (@lawyer.admin?)
+      redirect_to root_url
+    end
+
     if (@lawyer.nil?)
       flash[:alert] = 'Lawyer with #{params[:id]} not found.'
       redirect_to lawyers_path
     end
+
   end
 
 end
