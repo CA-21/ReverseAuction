b3a7e3e3ba15d1982e4181189806cdc01241e4a4
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-16 23:14:09 +0530

    Adds lawyer validations

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -4,13 +4,20 @@ class Lawyer < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { minimum: 10, maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :registration_number, presence: true
   validates :address, presence: true
   validates :phone, presence: true
   validates :city, presence: true
 
+
   has_many :answers, dependent: :destroy
+
+  def to_s
+    "#{self.name}, #{self.city}, #{self.registration_number}"
+  end
 
   def admin?
     self.admin
--- spec/models/lawyer_spec.rb
+++ spec/models/lawyer_spec.rb
@@ -1,13 +1,46 @@
 require 'spec_helper'
 
 describe Lawyer do
-  it "is valid with a name"
 
-  it "is invalid without a name"
+  before(:each) do
+    @lawyer = Lawyer.new(name: 'Some Tester', email: 'someone@example.com', registration_number: 'something', address: 'some road in some city', city: 'nowhere', phone: '123456789', password: 'password123')
+  end
 
-  it "is invalid without an email address"
+  it 'is valid with a name' do
+    expect(@lawyer).to be_valid
+  end
 
-  it "is invalid with a duplicate email address"
+  it 'is invalid without a name' do
+    @lawyer.name = nil
+    expect(@lawyer).not_to be_valid
+  end
 
-  it "returns name, city, and registration as a string"
+  it 'has reasonable length for name' do
+    @lawyer.name = 'd' * 9
+    expect(@lawyer).not_to be_valid
+
+    @lawyer.name = 'a' * 51
+    expect(@lawyer).not_to be_valid
+  end
+
+  it 'is invalid without an email address' do
+    @lawyer.email = nil
+    expect(@lawyer).not_to be_valid
+  end
+
+  it 'is invalid with a duplicate email address' do
+    @lawyer.save
+
+    lawyer2 = Lawyer.new(name: 'Some Tester', email: 'someone@example.com', registration_number: 'something', address: 'some road in some city', city: 'nowhere', phone: '123456789', password: 'password123')
+    expect(lawyer2).not_to be_valid
+  end
+
+  it 'returns name, city, and registration as a string' do
+    expect(@lawyer.to_s).to eq 'Some Tester, nowhere, something'
+  end
+
+  it 'does not allow invalid email addresses' do
+    @lawyer.email = 'sometester'
+    expect(@lawyer).not_to be_valid
+  end
 end
