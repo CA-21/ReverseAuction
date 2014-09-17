5c6449cd8efbb190427e8d37827cffe00520ff9c
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-17 13:46:22 +0530

    Adds tests for About and Contact pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,23 @@ require 'spec_helper'
 describe "static pages" do
 
   describe "home page" do
-
     it 'should have the content Ask Question' do
       visit root_path
       expect(page).to have_content('Ask Question')
     end
+  end
 
+  describe 'contact page' do
+    it 'should have the content Contact' do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+  end
 
+  describe 'about page' do
+    it 'should have the content About' do
+      visit about_path
+      expect(page).to have_content('About')
+    end
   end
 end
