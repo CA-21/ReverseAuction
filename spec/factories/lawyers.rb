c6256fc2dfb0d329df132e6db04a3972da65c712
Aamir Sayid <aamir.sayid@gmail.com>
2014-09-18 22:37:55 +0530

    Adds factory for Lawyer model

--- spec/factories/lawyers.rb
+++ spec/factories/lawyers.rb
@@ -2,5 +2,12 @@
 
 FactoryGirl.define do
   factory :lawyer do
+    name 'Some Tester'
+    sequence(:email) { |n| "some_tester#{n}@example.com" }
+    registration_number 'something'
+    address 'some road in some city'
+    city 'nowhere'
+    phone '123456789'
+    password 'password123'
   end
 end
